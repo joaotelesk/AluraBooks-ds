ce4320aea1c3494df83cfecb028cd7d404e87a3a
Joao Teles <joaojoseteles@gmail.com>
2022-11-26 18:36:04 -0300

    :sparkles: feat: component CampoTextos e stories

---
+++ stories/AbCampoTexto.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { AbCampoTexto, CampoTextoProps } from '../src/componentes/AbCampoTexto';
+
+export default {
+  title: 'componentes/AbCampoTexto',
+  component: AbCampoTexto,
+  argTypes: {
+    label: {
+      control: 'text',
+    },
+  },
+  parameters: { actions: { argTypesRegex: '^on.*' } },
+} as ComponentMeta<typeof AbCampoTexto>;
+
+const Template: ComponentStory<typeof AbCampoTexto> = args => (
+  <AbCampoTexto {...args} />
+);
+
+export const AbCampoTextoComponent = Template.bind({});
+AbCampoTextoComponent.args = {
+  label: 'Uma label interessante',
+} as CampoTextoProps;
