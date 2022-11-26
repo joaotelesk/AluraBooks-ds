90036f0f654ad13c4c995c51a5e208ce6fec40f5
Joao Teles <joaojoseteles@gmail.com>
2022-11-26 15:03:39 -0300

    :bulb: docs: teste para retirar erros de CI

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,9 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { AbBotao } from '../.';
 
 const App = () => {
-  return (
-    <div>
-      <AbBotao />
-    </div>
-  );
+  return <div></div>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
